79c0ded91b4d425b877792138056fce07326dcd8
Stephen Goodman <Steve@Stephens-MacBook-Air.local>
2015-09-06 14:44:47 -0500

    added constants file.

---
+++ ImageNear/Model/IMAConstants.h
@@ -0,0 +1,16 @@
+//
+//  IMAConstants.h
+//  ImageNear
+//
+//  Created by Stephen Goodman on 9/6/15.
+//  Copyright (c) 2015 Stephen Goodman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface IMAConstants : NSObject
+
+extern NSString *const WebServiceBaseURL;
+
+@end
---
+++ ImageNear/Model/IMAConstants.m
@@ -0,0 +1,15 @@
+//
+//  IMAConstants.m
+//  ImageNear
+//
+//  Created by Stephen Goodman on 9/6/15.
+//  Copyright (c) 2015 Stephen Goodman. All rights reserved.
+//
+
+#import "IMAConstants.h"
+
+@implementation IMAConstants
+
+NSString *const WebServiceBaseURL = @"http://www.panoramio.com/map/";
+
+@end
